15d47a429aaa8c42a3da31fddf887bcec42fbda7
Robert Recalo <rrecalo3@ggc.edu>
2023-08-23 10:17:29 -0400

    Added gain labels to right side of response curve area

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -393,6 +393,27 @@ void ResponseCurveComponent::resized()
         r.setY(1);
         
         g.drawFittedText(str, r, juce::Justification::centred, 1);
+    }
+    
+    for( auto gDb : gain )
+    {
+        auto y = jmap(gDb, -24.f, 24.f, float(bottom), float(top));
+        
+        String str;
+        if( gDb > 0 )
+            str << "+";
+        str << gDb;
+        
+        auto textWidth = g.getCurrentFont().getStringWidth(str);
+        
+        Rectangle<int> r;
+        r.setSize(textWidth, fontHeight);
+        r.setX(getWidth() - textWidth);
+        r.setCentre(r.getCentreX(), y);
+        
+        g.setColour(gDb == 0.f ? Colour(0u, 172u, 1u) : Colours::lightgrey);
+        
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
         
     }
     
